3a9a9081c491bb92ea09cc0158525e5dfe5ac5c4
githubdewill <hd209458inj@gmail.com>
2019-06-11 15:07:56 -0400

    fix migration bugs

--- templates/__init__.py
+++ templates/__init__.py
@@ -1,7 +1,7 @@
-from templates.home.views import half_blueprint
+from templates.home.views import home_blueprint
 from flask import Flask
 app = Flask(__name__,
             static_folder='./public',
             template_folder="./static/templates")
 # register the blueprints
-app.register_blueprint(half_blueprint)
+app.register_blueprint(home_blueprint)
--- templates/home/views.py
+++ templates/home/views.py
@@ -1,7 +1,28 @@
 from flask import render_template, Blueprint
-half_blueprint = Blueprint('halfnhalf',__name__)
+home_blueprint = Blueprint('home',__name__)
 
-@half_blueprint.route('/')
-@half_blueprint.route('/halfnhalf')
+@home_blueprint.route('/')
+@home_blueprint.route('/home')
 def index():
 	return render_template("index.html")
+
+
+@home_blueprint.route('/visualization_i2i')
+def visualization_i2i():
+    return render_template('visualization_i2i.html')
+
+@home_blueprint.route('/visualization_i2l')
+def visualization_i2l():
+    return render_template('visualization_i2l.html')
+
+@home_blueprint.route('/visualization_l2i')
+def visualization_l2i():
+    return render_template('visualization_l2i.html')
+
+@home_blueprint.route('/dataset')
+def dataset():
+    return render_template('dataset.html')
+
+@home_blueprint.route('/people')
+def people():
+    return render_template('people.html')
